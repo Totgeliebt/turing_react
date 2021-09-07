41e9ed7a1d27a24efd16f6cddbbcc1d43c9e451a
zhuravlik <Zlaska@yandex.ru>
2021-09-07 11:09:01 +0300

    exercise 19

---
+++ src/Homework/19exercise/index.js
@@ -0,0 +1,42 @@
+import TextAreaFirst from '../19exercise/first'
+import InputSecond from '../19exercise/first'
+import TwoInputsThird from '../19exercise/first'
+import OrderedListFourth from '../19exercise/first'
+import FormSixth from '../19exercise/first'
+import CheckboxSeventh from '../19exercise/first'
+import SelectEighth from '../19exercise/first'
+
+const ShowHomework = () => {
+    return(
+        <>
+            <TextAreaFirst/>
+            <br/>
+            <br/>
+            <InputSecond/>
+            <br/>
+            <br/>
+            <TwoInputsThird/>
+            <br/>
+            <br/>
+            <OrderedListFourth/>
+            <br/>
+            <br/>
+            <FormSixth/>
+            <br/>
+            <br/>
+            <CheckboxSeventh/>
+            <br/>
+            <br/>
+            <SelectEighth/>
+        </>
+    )
+}
+export default ShowHomework
+
+
+
+
+
+
+
+
