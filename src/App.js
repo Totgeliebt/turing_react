bb573ff63089ce205afcd480b281ea51887a3b04
zhuravlik <Zlaska@yandex.ru>
2021-09-16 18:28:50 +0300

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-
 import React from 'react'
-import {Link, Route, Switch} from 'react-router-dom'
-import ToDo from './pages/ToDo'
-import ImagesGallery from "./pages/ImagesGallery";
+import ShowHomework20 from "./Homework/20exercise";
 
 class App extends React.Component {
     render() {
         return (
-            <>
-                <Link to="/">Open ToDo page</Link>
-                <Link to="/images_gallery">Images gallery</Link>
-                <Link to="/blocked page">Blocked page</Link>
-                <Switch>
-                    <Route path="/blocked_page">
-                        <h1>Blocked page</h1>
-                    </Route>
-                    <Route path="/images_gallery">
-                        <ImagesGallery/>
-                    </Route>
-                    <Route path="/">
-                        <ToDo/>
-                    </Route>
-                </Switch>
-            </>
+            <ShowHomework20/>
         )
     }
 }
--- src/features/counter/CounterShow.js
+++
@@ -1,9 +0,0 @@
-import { useSelector} from 'react-redux'
-
-const CounterShow = () => {
-    const count = useSelector((state) => state.counter.value)
-    return (
-        <div>Show counter: {count}</div>
-    )
-}
-export default CounterShow
